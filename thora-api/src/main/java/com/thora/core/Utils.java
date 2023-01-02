2aa105a785a24b9f2dd9ffa93b27b5bf51badddd
flatbmx <david@podts.com>
2023-01-02 01:55:27 -0600

    Implement JVM-wide Thread stacktrace dump

--- thora-api/src/main/java/com/thora/core/Utils.java
+++ thora-api/src/main/java/com/thora/core/Utils.java
@@ -18,45 +18,43 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.apache.logging.log4j.Logger;
-
 public final class Utils {
-	
+
 	public static class Timer {
-		
+
 		public static final int DEFAULT_HISTORY_SIZE = 4;
 		public static final int DEFAULT_HISTORY_GROWTH_SIZE = 0;
-		
+
 		public static String pretty(long time) {
 			return time + "ms";
 		}
-		
+
 		private AtomicBoolean atomicStarted = new AtomicBoolean();
 		private boolean started;
 		private long startTime;
 		private final List<Long> marks, safeMarks;
-		
+
 		public Timer() {
 			this(DEFAULT_HISTORY_SIZE, DEFAULT_HISTORY_GROWTH_SIZE);
 		}
-		
+
 		public Timer(int initialSize, int growthIncrement) {
 			marks = new Vector<>(initialSize, growthIncrement);
 			safeMarks = Collections.unmodifiableList(marks);
 		}
-		
+
 		protected long getCurrentTime() {
 			return System.currentTimeMillis();
 		}
-		
+
 		public final boolean hasStarted() {
 			return started;
 		}
-		
+
 		public long getStartTime() {
 			return startTime;
 		}
-		
+
 		public Timer start() {
 			if(atomicStarted.compareAndSet(false, true)) {
 				startTime = getCurrentTime();
@@ -64,7 +62,7 @@ public final class Utils {
 			}
 			return this;
 		}
-		
+
 		public long mark() {
 			if(!hasStarted())
 				throw new IllegalStateException(this + " cannot call mark() until start() is called!");
@@ -72,11 +70,11 @@ public final class Utils {
 			marks.add(timeDiff);
 			return timeDiff;
 		}
-		
+
 		public String prettyMark() {
 			return pretty(mark());
 		}
-		
+
 		public long nextMark() {
 			if(!hasStarted())
 				throw new IllegalStateException(this + " cannot call mark() until start() is called!");
@@ -86,55 +84,55 @@ public final class Utils {
 				timeDiff -= marks.get(marks.size()-2);
 			return timeDiff;
 		}
-		
+
 		public String nextPrettyMark() {
 			return pretty(nextMark());
 		}
-		
+
 		public List<Long> getMarks() {
 			return safeMarks;
 		}
-		
+
 		public Stream<Long> marks() {
 			return getMarks().stream();
 		}
-		
+
 		public long getMark(int occurrence) {
 			return getMarks().get(occurrence);
 		}
-		
+
 		public Collection<Long> getMarkTimes() {
 			return markTimes()
 					.collect(Collectors.toList());
 		}
-		
+
 		public Stream<Long> markTimes() {
 			return marks()
 					.map(this::computeMarkDifference);
 		}
-		
+
 		protected final long computeMarkDifference(long mark) {
 			return mark - startTime;
 		}
-		
+
 		public long getMarkTime(int occurrence) {
 			return getMark(occurrence) - startTime;
 		}
-		
+
 	}
-	
+
 	public static void assertNull(Object o, String message) {
 		if(o == null) throw new NullPointerException(message);
 	}
-	
+
 	public static void assertNull(Object o, Supplier<String> messageSupplier) {
 		if(o == null) throw new NullPointerException(messageSupplier.get());
 	}
-	
+
 	public static void assertNullArg(Object o, String message) {
 		assertArg(o == null, message);
 	}
-	
+
 	/**
 	 * Throws a new IllegalArgumentException if the given condition is true.
 	 * @param condition the condition.
@@ -143,7 +141,7 @@ public final class Utils {
 	public static void assertArg(boolean condition, String message) {
 		if(condition) throw new IllegalArgumentException(message);
 	}
-	
+
 	/**
 	 * Throws a new IllegalArgumentException if the given condition is true.
 	 * @param bool the condition
@@ -152,7 +150,7 @@ public final class Utils {
 	public static void assertArg(boolean bool, Supplier<String> messageSupplier) {
 		if(bool) throw new IllegalArgumentException(messageSupplier.get());
 	}
-	
+
 	public static <E> Collection<E> intersection(Collection<E> a, Collection<E> b) {
 		Collection<E> intersection = new ArrayList<>();
 		Collection<E> smaller = a, bigger = b;
@@ -160,14 +158,14 @@ public final class Utils {
 			smaller = b;
 			bigger = a;
 		}
-		
+
 		for(E e : smaller) {
 			if(bigger.contains(e))
 				intersection.add(e);
 		}
 		return intersection;
 	}
-	
+
 	/**
 	 * Returns a Stream that consists of only the elements from the fist 
 	 * Collection and not the second.
@@ -180,11 +178,11 @@ public final class Utils {
 		return core.stream()
 				.filter(e -> !removals.contains(e));
 	}
-	
+
 	public static <E> Collection<E> getMissing(Collection<E> c, Collection<E> removals) {
 		return missing(c, removals).collect(Collectors.toList());
 	}
-	
+
 	public static final <E> int empty(Iterable<E> all, Consumer<? super E> consumer) {
 		int count = 0;
 		Iterator<E> it = all.iterator();
@@ -196,7 +194,7 @@ public final class Utils {
 		}
 		return count;
 	}
-	
+
 	public static final <E> int empty(Queue<E> queue, Consumer<? super E> consumer) {
 		int count = 0;
 		E e;
@@ -206,7 +204,7 @@ public final class Utils {
 		}
 		return count;
 	}
-	
+
 	public static final <K,V> int empty(Map<K,V> map, Consumer<? super V> consumer) {
 		int count = 0;
 		Iterator<Entry<K,V>> it = map.entrySet().iterator();
@@ -218,13 +216,13 @@ public final class Utils {
 		}
 		return count;
 	}
-	
+
 	public static final <L,A,B> void trigger(Iterable<L> all, Function<L,BiPredicate<? super A,? super B>> mapping, A a, B b) {
 		trigger(all, l -> {
 			return mapping.apply(l).test(a, b);
 		});
 	}
-	
+
 	public static final <L,A> void trigger(Iterable<L> all, Function<L,Predicate<? super A>> mapping, A element) {
 		Iterator<L> it = all.iterator();
 		while(it.hasNext()) {
@@ -233,7 +231,7 @@ public final class Utils {
 				it.remove();
 		}
 	}
-	
+
 	/**
 	 * Calls a listener function over all elements in a Iterable.
 	 * Each listener in the Iterable where they return {@code true} will be removed from the Iterable.
@@ -254,7 +252,7 @@ public final class Utils {
 		}
 		return count;
 	}
-	
+
 	public static final <K,V> int trigger(Map<K,V> map, Predicate<? super V> pred) {
 		int count = 0;
 		Iterator<Entry<K,V>> it = map.entrySet().iterator();
@@ -267,23 +265,45 @@ public final class Utils {
 		}
 		return count;
 	}
-	
+
 	public static final <L extends BiPredicate<? super A,? super A>,A> void triggerChange(Iterable<L> all, A from, A to) {
 		trigger(all, l -> l.test(from, to));
 	}
-	
+
 	public static final <L,A> void triggerChange(Iterable<L> all, Function<L,BiPredicate<? super A,? super A>> mapping, A from, A to) { 
 		trigger(all, l -> mapping.apply(l).test(from, to));
+	}
+
+	public static final String newThreadDumpMessage() {
+		StringBuilder sb = new StringBuilder();
+		Collection<Entry<Thread, StackTraceElement[]>> threads = Thread.getAllStackTraces().entrySet();
+		for(Entry<Thread, StackTraceElement[]> e: threads) {
+			final Thread t = e.getKey();
+			final StackTraceElement[] stackTraces = e.getValue();
+			
+			sb.append("Thread: ").append(t.getName())
+			.append(" | PID: ").append(t.getId())
+			.append(" | State: ").append(t.getState())
+			.append(System.lineSeparator());
+			
+			for(int i=0; i<stackTraces.length; ++i) {
+				final StackTraceElement traceElement = stackTraces[i];
+				sb.append("\t").append(traceElement)
+				.append(System.lineSeparator());
+			}
+		}
+
+		return sb.toString();
 	}
 	
 	public static final String getRenamedPackageClass(Class<?> c, String newClassName) {
 		return c.getPackage().getName() + "." + newClassName;
 	}
-	
+
 	public static final String simpleClassName(Object o) {
 		return o.getClass().getSimpleName();
 	}
-	
+
 	private Utils() {}
-	
+
 }
